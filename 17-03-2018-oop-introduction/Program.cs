60930be1d67a7623f5ca3dc65ddb0fc7005df082
Samir Kerimov <gunayisg@gmail.com>
2018-03-17 15:17:37 +0400

    OOP in C# basics

---
+++ 17-03-2018-oop-introduction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_03_2018_oop_introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
