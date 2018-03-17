778fcb6b0d3bba600732b7b64ec5e5345a9aa3cb
Samir Kerimov <gunayisg@gmail.com>
2018-03-17 19:53:58 +0400

    class,object,abstraction

--- 17-03-2018-oop-introduction/Program.cs
+++ 17-03-2018-oop-introduction/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _17_03_2018_oop_introduction
 {
@@ -10,7 +7,46 @@ namespace _17_03_2018_oop_introduction
     {
         static void Main(string[] args)
         {
+           
+           
+            for(int i = 0; i < 3; i++)
+            {
+                Console.Write("Kitab adini daxil et: ");
+                var ad = Console.ReadLine();
+                Console.Write("Kitab yazarini daxil et: ");
+                var yazar = Console.ReadLine();
+                Kitab book = new Kitab(ad, yazar);
+                Kitab.db.Add(book);
+            }
+            Kitab.Print();
 
         }
+
+    }
+
+
+    class Kitab
+    {
+
+        public static List<Kitab> db = new List<Kitab>();
+        public string Name;
+        public string Author;
+     
+
+        public Kitab(string _name,string _author)
+        {
+            Name = _name;
+            Author = _author;
+           
+        }
+
+        public static void Print()
+        {
+            foreach (var item in db)
+            {
+                Console.WriteLine(item.Name +"/"+item.Author);
+            }
+        }
+       
     }
 }
